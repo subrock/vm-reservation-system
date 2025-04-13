f18e259aebe8f6615fcaecf3f3aedd0e0420e5ea
Karl Schroeder <subrock@users.noreply.github.com>
2025-04-13 14:06:00 -0700

    Add files via upload

---
+++ config/mail.js
@@ -0,0 +1,10 @@
+// config/mail.js
+module.exports = {
+  service: 'gmail.com', // e.g., 'Gmail', 'Outlook'
+  auth: {
+    user: 'subrock@gmail.com', // Your email address
+    pass: 'ilydytjemrnejonq'    // Your email password or app password
+  }
+};
+
+
